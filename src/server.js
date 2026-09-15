de180ec92782ca90b85f02636ea19b70539a980b
pasq47 <pasq47@hotmail.it>
2026-09-15 14:24:08 +0200

    test

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const barcodeRouter = require("./routes/barcode");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const connString = "postgresql://postgres.cgkgkfcsorvelzhspgbe:sumisumarinella@aws-0-eu-central-1.pooler.supabase.com:5432/postgres" //process.env.DATABASE_URL
+const connString = process.env.DATABASE_URL
 const isSecure = false //process.env.NODE_ENV === 'production';
 const pool = new Pool({
   connectionString: connString, 
